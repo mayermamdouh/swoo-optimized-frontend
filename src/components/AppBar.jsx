1cbf7dd1aaf9d042cba25a7ee78c4a291596d46e
mayermamdouh <m.noshy485@gmail.com>
2025-11-27 14:10:14 +0200

    this is the first commit

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+
+export default function AppBar() {
+  return (
+    <header className="flex flex-col">
+        <div className="bg-foreground ">
+            <Image src='/images/logoo.png' width={100} height={30}></Image>
+        <input className=""></input>
+        </div>
+
+      <nav></nav>
+    </header>
+  )
+}
